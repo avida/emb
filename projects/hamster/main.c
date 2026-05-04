11c9d039203f206a222fc93e670474b6e49e8fe6
Dmytro <avida.d3@gmail.com>
2026-05-04 15:45:55 +0300

    Simple external interrupt handler

--- projects/hamster/main.c
+++ projects/hamster/main.c
@@ -1,11 +1,32 @@
+
 #include <Arduino.h>
 
-#define SERVO_PIN 9
+#define INTERRUPT_PIN 2 // Use digital pin 2 for external interrupt (INT0 on most AVRs)
+
+volatile bool pin_high = false;
+
+void handleInterrupt() {
+    if (digitalRead(INTERRUPT_PIN) == HIGH) {
+        pin_high = true;
+    }
+}
+
+
 int main(void)
 {
     init();
     Serial.begin(57600);
     Serial.println("Hi");
+
+    pinMode(INTERRUPT_PIN, INPUT);
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), handleInterrupt, RISING);
+
+    while (1) {
+        if (pin_high) {
+            Serial.println("Interrupt: pin is HIGH");
+            pin_high = false;
+        }
+    }
 
     return 0;
 }
